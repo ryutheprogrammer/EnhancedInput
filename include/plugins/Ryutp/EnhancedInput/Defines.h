655785808988ff7b70fb2ce2a07e0eb050927a3f
ryutp <ryutp@unigine.com>
2026-03-31 20:36:44 +0700

    Fix msvc ENUM

--- include/plugins/Ryutp/EnhancedInput/Defines.h
+++ include/plugins/Ryutp/EnhancedInput/Defines.h
@@ -35,7 +35,7 @@
 #define INTERNAL_ARGS_AUGMENTER(...) unused, __VA_ARGS__
 #define INTERNAL_EXPAND(x) x
 #define INTERNAL_EXPAND_ARGS_PRIVATE(...) INTERNAL_EXPAND(INTERNAL_GET_ARG_COUNT_PRIVATE(__VA_ARGS__, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0))
-#define INTERNAL_GET_ARG_COUNT_PRIVATE(_1_, _2_, _3_, _4_, _5_, _6_, _7_, _8_, _9_, _10_, _11_, _12_, _13_, _14_, _15_, _16_, count, ...) count
+#define INTERNAL_GET_ARG_COUNT_PRIVATE(_0_, _1_, _2_, _3_, _4_, _5_, _6_, _7_, _8_, _9_, _10_, _11_, _12_, _13_, _14_, _15_, _16_, count, ...) count
 
 #else // Non-Microsoft compilers
 
